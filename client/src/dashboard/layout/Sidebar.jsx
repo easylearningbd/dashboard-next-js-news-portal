74e3a15e671a153a9e65c7751173744190144f40
easylearningbd <elebdsoft@gmail.com>
2024-09-15 03:13:04 +0600

    n7 Admin Panel Design Part 3

--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 const Sidebar = () => {
     return (
-        <div>
-            <h1>Sidebar</h1>
+        <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#dadaff]'>
+            <div className='h-[70px] flex justify-center items-center'>
+                <Link to='/'>
+            <img className='w-[190px] h-[35px]' src="https://i.ibb.co.com/WcB36Jq/mainlogo.png" alt="" />
+                </Link>
+
+            </div>
+           
         </div>
     );
 };
