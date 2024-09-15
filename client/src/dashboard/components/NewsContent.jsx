87ab8992afaf992a8e5a153559b11aa938ac02bc
easylearningbd <elebdsoft@gmail.com>
2024-09-16 01:55:30 +0600

    n14 News Page Design Part 4

--- client/src/dashboard/components/NewsContent.jsx
+++ client/src/dashboard/components/NewsContent.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import profile from '../../assets/profile.png'
+import { Link } from 'react-router-dom';
+import { FaEye } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
 
 const NewsContent = () => {
 
@@ -15,21 +20,53 @@ const NewsContent = () => {
     </div>
 
     <div className='overflow-x-auto'>
-        <table className='w-full table-auto bg-white shadow-lg rounded-lg overflow-hidden'>
-            <thead className='bg-gray-100 text-gray-700 uppercase text-sm'>
-                <tr>
-                    <th className='py-4 px-6 text-left'>No</th>
-                    <th className='py-4 px-6 text-left'>Title</th>
-                    <th className='py-4 px-6 text-left'>Image</th>
-                    <th className='py-4 px-6 text-left'>Category</th>
-                    <th className='py-4 px-6 text-left'>Description</th>
-                    <th className='py-4 px-6 text-left'>Date</th>
-                    <th className='py-4 px-6 text-left'>Status</th>
-                    <th className='py-4 px-6 text-left'>Action</th>
-                </tr>
+<table className='w-full table-auto bg-white shadow-lg rounded-lg overflow-hidden'>
+    <thead className='bg-gray-100 text-gray-700 uppercase text-sm'>
+        <tr>
+            <th className='py-4 px-6 text-left'>No</th>
+            <th className='py-4 px-6 text-left'>Title</th>
+            <th className='py-4 px-6 text-left'>Image</th>
+            <th className='py-4 px-6 text-left'>Category</th>
+            <th className='py-4 px-6 text-left'>Description</th>
+            <th className='py-4 px-6 text-left'>Date</th>
+            <th className='py-4 px-6 text-left'>Status</th>
+            <th className='py-4 px-6 text-left'>Action</th>
+        </tr> 
+    </thead>
+    <tbody className='text-gray-600'>
+        {[1,2,3].map((item,index) => (
+            <tr key={index} className='border-t'>
+                <td className='py-4 px-6'>1</td>
+                <td className='py-4 px-6'>News Title</td>
+                <td className='py-4 px-6'>
+                    <img className='w-10 h-10 rounded-full object-cover' src={profile} alt="news" />
+                </td>
+                <td className='py-4 px-6'>Category Name</td>
+                <td className='py-4 px-6'>Description</td>
+                <td className='py-4 px-6'>12-08-2024</td>
+                <td className='py-4 px-6'>
+                    <span className='px-3 py-1 bg-green-200 rounded-full text-xs font-semibold'>Active</span>
+                </td>
+                <td className='py-4 px-6'>
+                    <div className='flex gap-3 text-gray-500'>
+                        <Link to="#" className='p-2 bg-blue-500 text-white rounded hover:bg-blue-800'>
+                        <FaEye />
+                        </Link> 
+                        <Link to="#" className='p-2 bg-yellow-500 text-white rounded hover:bg-yellow-800'>
+                        <FaEdit />
+                        </Link> 
+                        <Link to="#" className='p-2 bg-red-500 text-white rounded hover:bg-red-800'>
+                        <FaTrashAlt />
+                        </Link> 
+                    </div>
+                </td>
+            </tr>
+        ))
 
-            </thead>
-        </table>
+        }
+
+    </tbody>
+</table>
 
     </div>
     
