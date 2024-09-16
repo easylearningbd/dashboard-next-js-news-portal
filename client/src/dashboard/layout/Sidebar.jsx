6dff538a8699ea338bbfb3c72a5600f5fa0f2493
easylearningbd <elebdsoft@gmail.com>
2024-09-16 17:48:08 +0600

    n24 Writer Add News Page Setup Part 2

--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -9,7 +9,10 @@ import { IoMdAdd } from "react-icons/io";
 
 const Sidebar = () => {
     const { pathname } = useLocation();
-    console.log(pathname);
+    // console.log(pathname);
+    const userInfo = {
+        role: "writer"
+      } 
 
     return (
         <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#dadaff]'>
@@ -19,24 +22,12 @@ const Sidebar = () => {
                 </Link> 
             </div>
 <ul className='px-3 flex flex-col gap-y-1 font-medium'>
-    <li>
+    {
+        userInfo.role === 'admin' ? <>
+        <li>
         <Link to='/dashboard/admin' className={`px-3 ${pathname === '/dashboard/admin' ? 'bg-indigo-500 text-white' : 'bg-white text-[#404040f6]'} py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-indigo-500 hover:text-white `} >
         <span className='text-[18px]'><MdDashboard /></span>
         <span className='text-[18px]'>Dashboard</span>
-        </Link>
-    </li>
-
-    <li>
-        <Link to='/dashboard/news' className={`px-3 ${pathname === '/dashboard/news' ? 'bg-indigo-500 text-white' : 'bg-white text-[#404040f6]'} py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-indigo-500 hover:text-white `} >
-        <span className='text-[18px]'><BiNews /></span>
-        <span className='text-[18px]'>News</span>
-        </Link>
-    </li>
-
-    <li>
-        <Link to='/dashboard/news/create' className={`px-3 ${pathname === '/dashboard/news/create' ? 'bg-indigo-500 text-white' : 'bg-white text-[#404040f6]'} py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-indigo-500 hover:text-white `} >
-        <span className='text-[18px]'><IoMdAdd  /></span>
-        <span className='text-[18px]'>Add News</span>
         </Link>
     </li>
 
@@ -53,7 +44,33 @@ const Sidebar = () => {
         <span className='text-[18px]'>Writers</span>
         </Link>
     </li>
+        
+        </> : <>
+        <li>
+        <Link to='/dashboard/writer' className={`px-3 ${pathname === '/dashboard/writer' ? 'bg-indigo-500 text-white' : 'bg-white text-[#404040f6]'} py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-indigo-500 hover:text-white `} >
+        <span className='text-[18px]'><MdDashboard /></span>
+        <span className='text-[18px]'>Dashboard</span>
+        </Link>
+    </li>
 
+    <li>
+        <Link to='/dashboard/news/create' className={`px-3 ${pathname === '/dashboard/news/create' ? 'bg-indigo-500 text-white' : 'bg-white text-[#404040f6]'} py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-indigo-500 hover:text-white `} >
+        <span className='text-[18px]'><IoMdAdd  /></span>
+        <span className='text-[18px]'>Add News</span>
+        </Link>
+    </li>
+        
+        </>
+    }
+ 
+
+    <li>
+        <Link to='/dashboard/news' className={`px-3 ${pathname === '/dashboard/news' ? 'bg-indigo-500 text-white' : 'bg-white text-[#404040f6]'} py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-indigo-500 hover:text-white `} >
+        <span className='text-[18px]'><BiNews /></span>
+        <span className='text-[18px]'>News</span>
+        </Link>
+    </li>
+ 
     <li>
         <Link to='/dashboard/profile' className={`px-3 ${pathname === '/dashboard/profile' ? 'bg-indigo-500 text-white' : 'bg-white text-[#404040f6]'} py-2 hover:shadow-lg hover:shadow-indigo-500/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-indigo-500 hover:text-white `} >
         <span className='text-[18px]'><FaHouseUser   /></span>
--- client/src/dashboard/pages/CreateNews.jsx
+++ client/src/dashboard/pages/CreateNews.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CreateNews = () => {
     return (
-        <div>
-            Create News 
+        <div className='bg-white shadow-md rounded-md p-6'>
+            <div className='flex justify-between items-center mb-6'>
+                <h2 className='text-2xl font-bold text-gray-700'>Add News</h2>
+                <Link className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-800 transition duration-300' to='/dashboard/news'>View All
+                </Link>
+
+            </div>
+             
         </div>
     );
 };
