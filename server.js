ce7ef9bf2455addc4f664bd4056d943700d6ff0e
easylearningbd <elebdsoft@gmail.com>
2024-09-17 01:11:00 +0600

    n31 Login Page Design and DB Connect Part 4

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const db_connect = require('./utils/db')
 
 dotenv.config()
 
 const port = process.env.port
 
 app.get('/', (req,res) => res.send("Hello Easy"))
+
+db_connect()
+
 app.listen(port,() => console.log(`Server is running on port ${port}`))
