65149bd3a2c5628f5deb713b38a58dfb14d25028
easylearningbd <elebdsoft@gmail.com>
2024-09-17 03:58:13 +0600

    n35 User Login Backend Part 4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import StorePovider from './context/StorePovider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StorePovider>
+    <>
     <App />
-  </StrictMode>,
+    <Toaster/>
+    </>
+  </StorePovider>,
 )
