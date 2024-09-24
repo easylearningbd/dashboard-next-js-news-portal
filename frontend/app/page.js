3ff3dc4864b6c3e7e392a29761828e9d8bb5cdfa
easylearningbd <elebdsoft@gmail.com>
2024-09-25 04:02:31 +0600

    81 Frontend Headline Design

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,10 @@
+import HeadLines from "@/components/HeadLines";
 import Image from "next/image";
 
 export default function Home() {
   return (
      <main>
+      <HeadLines/>
       <h1>Main Page </h1>
      </main>
   );
