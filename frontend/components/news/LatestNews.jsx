f48f0e4aaed3fcd739946dccac30d496dfea4e9b
easylearningbd <elebdsoft@gmail.com>
2024-09-25 05:41:01 +0600

    85 Latest News Design Part 4

--- frontend/components/news/LatestNews.jsx
+++ frontend/components/news/LatestNews.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import SimpleNewsCard from './item/SimpleNewsCard';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 const LatestNews = () => {
 
@@ -26,6 +27,28 @@ const LatestNews = () => {
         }
       };
 
+    const ButtonGroup = ({ next, previous }) => {
+        return (
+            <div className='flex justify-between items-center py-2'>
+                <div className='text-xl font-bold text-gray-800 relative pl-4'>
+                    <span className='absolute inset-y-0 left-0 w-1 bg-blue-600 rounded-sm'></span>
+                    Latest News 
+                </div>
+        <div className='flex items-center space-x-2'>
+            <button onClick={() => previous()} className='w-9 h-9 flex items-center justify-center rounded-full bg-white text-gray-600 hover:bg-gray-200 hover:text-gray-800 shadow-md transition'>
+                <FiChevronLeft size={20}/> 
+            </button>
+
+            <button onClick={() => next()} className='w-9 h-9 flex items-center justify-center rounded-full bg-white text-gray-600 hover:bg-gray-200 hover:text-gray-800 shadow-md transition'>
+                <FiChevronRight size={20}/> 
+            </button>
+
+        </div> 
+
+            </div>
+        )
+    }
+
     return (
         <div className='w-full flex flex-col-reverse gap-3 pr-0 lg:pr-2'>
             <Carousel
@@ -33,6 +56,7 @@ const LatestNews = () => {
             arrows={false}
             renderButtonGroupOutside={true}
             responsive={responsive}
+            customButtonGroup={<ButtonGroup/>}
             infinite={true}
             transitionDuration={500}
             >
