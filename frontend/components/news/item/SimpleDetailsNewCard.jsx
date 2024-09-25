76379e86b065aa8179427e79223de1bb59337e71
easylearningbd <elebdsoft@gmail.com>
2024-09-26 03:53:22 +0600

    90 Popular News Design Part 4

--- frontend/components/news/item/SimpleDetailsNewCard.jsx
+++ frontend/components/news/item/SimpleDetailsNewCard.jsx
@@ -30,8 +30,7 @@ const SimpleDetailsNewCard = ({type}) => {
         </div> 
         { type === 'details_news' && 
         <p className='text-sm text-slate-600 pt-3'>
-The news that Margot Robbie and Jacob Elordi are starring in Emerald Fennell's take on Emily Brontë's romance has caused a storm – no surprise in an age of increased casting scrutiny.
-
+The news that Margot Robbie and Jacob Elordi are starring in Emerald Fennell's take on Emily Brontë's romance has caused a storm – no surprise in an age of increased casting scrutiny. 
         </p>  }
     </div>
     
