d316629e499b91b0df0058c320a6024eb9f6d4a5
easylearningbd <elebdsoft@gmail.com>
2024-10-03 02:54:13 +0600

    130 User Change Password Functions Part 1

--- client/src/dashboard/pages/Profile.jsx
+++ client/src/dashboard/pages/Profile.jsx
@@ -15,6 +15,10 @@ const Profile = () => {
     const [message, setMessage] = useState("")
     const [imageUrl, setImageUrl] = useState("") // Current Image 
 
+    const [oldPassword, setOldPassword] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+
 useEffect(() => {
     const fetchProfile = async () => {
         try {
@@ -67,6 +71,37 @@ useEffect(() => {
 
     }
 
+
+
+    // Handle Password update 
+    const handlePasswordChange =  async (e) => {
+        e.preventDefault();
+        //Validate Input
+        if (!oldPassword || !newPassword) {
+            setPasswordError("Please fill in both password fields");
+            return;
+        }
+        if (newPassword.length < 6) {
+            setPasswordError("New password must be at least 6 characters long");
+            return;
+        }
+
+        try {
+            const response = await axios.post(`${base_url}/api/change-password`,{oldPassword,newPassword},
+                 {
+                headers: {
+                    'Authorization' : `Bearer ${store.token}`
+                }
+            });
+            toast.success("Password Updaetd successfully");
+            setOldPassword("")
+            setNewPassword("")
+            setPasswordError("")
+        } catch (error) {
+            setPasswordError("Failed to update password")
+        } 
+    }
+
     return (
 <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-2 mt-5'>
     
@@ -75,6 +110,7 @@ useEffect(() => {
             {
                 imageUrl ? ( 
                     <img src={imageUrl} alt="Profile" className='w-[150px] h-[150px] rounded-full object-cover'/>
+                    
                 ) : (
                 <label htmlFor="img" className='w-[150px] h-[150px] flex flex-col justify-center items-center rounded-full bg-gray-200 border-2 border-dashed border-gray-300 text-gray-600 cursor-pointer hover:bg-gray-200 transition duration-300'>
                     <FaImage className='text-4xl' />
@@ -106,20 +142,20 @@ useEffect(() => {
 
 <div className='bg-white p-6 rounded-lg shadow-md text-gray-700'>
   <h2 className='text-lg font-bold text-center mb-5'>Change Password</h2>
-  <form>
+  <form onSubmit={handlePasswordChange}>
     <div className='space-y-4'>
         <div>
             <label htmlFor="old_password" className='block text-md font-semibold text-gray-600'>Old Password </label>
-            <input type="password" id='old_password' name='old_password' placeholder='Enter Old Passowrd' className='w-full px-3 py-2 mt-2 border border-gray-400 rounded-md focus:ring-2 focus:ring-blue-500 outline-none transition duration-300' />
+            <input type="password" id='old_password' name='old_password' value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} placeholder='Enter Old Passowrd' className='w-full px-3 py-2 mt-2 border border-gray-400 rounded-md focus:ring-2 focus:ring-blue-500 outline-none transition duration-300' />
         </div>
 
 
         <div>
             <label htmlFor="new_password" className='block text-md font-semibold text-gray-600'>New Password </label>
-            <input type="password" id='new_password' name='new_password' placeholder='Enter New Passowrd' className='w-full px-3 py-2 mt-2 border border-gray-400 rounded-md focus:ring-2 focus:ring-blue-500 outline-none transition duration-300' />
+            <input type="password" id='new_password' name='new_password' placeholder='Enter New Passowrd' value={newPassword} onChange={(e) => setNewPassword(e.target.value)}  className='w-full px-3 py-2 mt-2 border border-gray-400 rounded-md focus:ring-2 focus:ring-blue-500 outline-none transition duration-300' />
         </div> 
     </div>
-
+    {passwordError && <p className='text-center mt-4'>{passwordError}</p>}
     <div className='mt-6'>
         <button type='submit' className='w-full px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-800 transition duration-300'>
             Change Password
